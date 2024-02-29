1aaa9fa75d5d01bb49c4af2a036bbf5cd5e2686a
Erika <143980757+erikaatthacarya@users.noreply.github.com>
2024-02-29 20:09:13 +0700

    add PERTEMUAN 3

---
+++ CanteenDemo.java
@@ -0,0 +1,22 @@
+public class CanteenDemo {
+    public static void main(String[] args) {
+            item09 item1 = new item09();
+            item1.name = "Sate";
+            item1.price = 20000;
+            item1.stock = 20;
+            item1.displayInfo();
+
+            item09 item2 = new item09();
+            item2.name = "Bakso";
+            item2.price = 10000;
+            item2.stock = 10;
+            item2.displayInfo();
+
+            item09 item3 = new item09();
+            item3.name = "Sosis";
+            item3.price = 19000;
+            item3.stock = 10;
+            item3.displayInfo();
+    }
+}    
+
---
+++ item09.java
@@ -0,0 +1,14 @@
+/**
+ * item09
+ */
+public class item09 {
+    public static String name;
+    public static double price;
+    public static int stock;
+
+    public void displayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Stock: " + stock);
+    }
+}
